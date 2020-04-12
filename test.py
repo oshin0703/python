1b4059288003a02d855d34a49884bc3740399a10
oshin0703 <so275417500@gmail.com>
2020-04-12 15:19:33 +0900

    今までの学習したこと

--- test.py
+++ test.py
@@ -1,1 +1,1 @@
-print('HelloWorld')
+print('python-izm')
---
+++ test04.py
@@ -0,0 +1,25 @@
+print('python-izm')
+print("python-izm")
+
+test_str = """
+test_1
+test_2
+"""
+print(test_str)
+
+test_str = 'python'
+test_str = test_str + '_'
+test_str += 'izm'
+print(test_str)
+
+test_str = '012'
+test_str += '345'
+test_str += '678'
+test_str += '9'
+print(test_str)
+
+test_str ='012'*3
+print(test_str)
+
+test_integer = 100
+print(str(test_integer + ))
