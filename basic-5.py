9109efc49519f05de96d37dae8926b89dda0faf0
oshin0703 <so275417500@gmail.com>
2020-04-13 18:18:49 +0900

    基礎編

--- basic-5.py
+++ basic-5.py
@@ -7,3 +7,9 @@ test_class_1.test_method('1')
 from testmod import TestClass
 test_class_2 = TestClass()
 test_class_2.test_method('2')
+
+from datetime import datetime
+datetime.today()
+
+from datetime import datetime as d
+d.today()
---
+++ test_argv.py
@@ -0,0 +1,7 @@
+#コマンドライン引数
+import sys
+args = sys.argv
+print(args)
+print('第１引数：' + args[1])
+print('第２引数：' + args[2])
+print('第３引数：' + args[3])
