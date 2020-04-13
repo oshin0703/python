c0121f5afd71fc5a1a8c2c134d33008b303452dd
oshin0703 <so275417500@gmail.com>
2020-04-13 17:48:44 +0900

    基礎編

---
+++ testmod.py
@@ -0,0 +1,9 @@
+#インポートするクラス
+class TestClass:
+    def __init__(self):
+        print('create TestClass')
+
+    def test_method(self,val):
+        print('create test_me')
+        print(val)
+        
